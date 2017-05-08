cb3d9b420cb96819476093bb5c215bf8bed596b9
ckrountree <jmail0207@gmail.com>
2017-05-08 15:41:55 -0700

    beginning object, properties and function

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var storeOne = {
+    cookiesPerHour: 6.3,
+    cookiesMin: 23,
+    cookiesMax: 65,
+    name: 'PDX Airport',
+    calcCustomersPerHour: function () {},
+    calcCookiesPerHour: function ()
+     {
+        nameOne.name();
+
+        
+    }
+
+
+};
