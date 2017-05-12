f7f56facbcdd61267a6886d0a93c31e777791b17
ckrountree <jmail0207@gmail.com>
2017-05-11 22:43:09 -0700

    wrote my for loop

--- js/app.js
+++ js/app.js
@@ -1,104 +1,48 @@
 'use strict';
 
-// 1.  create where (id) the info is going.
-// 2.  create the element that the info goes into.
-// 3.  assign the info to that element.
-// 4.  append the created element to the id that we found.
-//  Always go smallest to biggest
-
-
-var getEl = document.getElementById('here')
-var unordList = document.createElement('ul')
-var list = document.createElement('li')
-list.innerText = 'hello, i\'m here';
-getEl.appendChild(unordList);
-unordList.appendChild(list); 
-// appending list to parent which is unordList
-
-// object literal
-var obj = {
-    first: '1st prop',
-    second: '2nd prop',
-    third: '3rd prop',
-    fourth: '4th prop'
-}
-
-// function/object constructor to simplify repetitive functions/objects
-
-var ObjConstructor = function (fir, sec, thi, fou){
-    this.table = fir;
-    this.second = sec;
-    this.third = thi;
-    this.fourth = fou;
-}
-//  above is a list of common properties for all ObjConstructor
-
-var firstObj = new ObjConstructor('mine', 'yours', 'ours', 'theirs');
-var livingRoom = new ObjConstructor('ikea', 'target', 'rug', 'sink');
-
-//  above is calling/placing parameters for each ObjConstructor (ie: store)
-
-console.log(firstObj)
-
-//  Object Prototype...
-// reference the object created previously (constructor) with a name for whatever it is I want to do (pluralization)
-// now inside the object, so use "this" again
-
-ObjConstructor.prototype.pluralization = function (x) {
-    this.fourth = this.fourth + 's';
-    // or for assignment...
-    this.table = Math.floor(Math.random() * 10) + x);
-
-};
-
-livingRoom.pluralize(23);
-
-
-
-
-//  Start of Monday assigment
-
-
-var storeOne = {
-
-    cookiesPerHour: 6.3,
-    customersHourlyMin: 23,
-    customersHourlyMax: 65,
-    name: 'PDX Airport',
-    calcCustomersPerHour: function () {
-        return Math.floor(Math.random() * (this.customersHourlyMax - this.customersHourlyMin)) + this.customersHourlyMin;
-    },
-
-    calcCookiesPerHour: function () {
-        this.cookiesPerHour = [];
-        for (var i = 0; i < 15; i++);
-        var randomCookies = Math.round(this.calcCustomersPerHour * this.cookiesPerHour);
-        this.randomCookies.push(this.cookiesPerHour);
-    }
-
-};
-
 //  List of common properties for all Stores
 
 var StoreFront = function (cookieNum, customersMin, customersMax, storeName) {
-    this.cookiesPerHour = cookieNum;
+    this.hourlyCookiesSold = cookieNum;
     this.customersHourlyMin = customersMin;
     this.customersHourlyMax = customersMax;
     this.name = storeName;
 }
 
-//  Calling the function for each store
+//  Calling the function for each store and creates a new object with the following propeties
 
-    var store1 = new StoreFront('6.3', '23', '65', 'PDX Airport');
-    var store2 = new StoreFront('3', '24', '1.2', 'Pioneer Square');
-    var store3 = new StoreFront('11', '38', '3.7', 'Powell\'s');
-    var store4 = new StoreFront('20', '38', '2.3', 'St. John\s');
-    var store5 = new StoreFront('2', '16', '4.6', 'Waterfront');
+    var store1 = new StoreFront(6.3, 23, 65, 'PDX Airport');
+    var store2 = new StoreFront(3, 24, 1.2, 'Pioneer Square');
+    var store3 = new StoreFront(11, 38, 3.7, 'Powell\'s');
+    var store4 = new StoreFront(20, 38, 2.3, 'St. John\s');
+    var store5 = new StoreFront(2, 16, 4.6, 'Waterfront');
 
-    //  Calculate the number of customers per hour and the number of cookies purchased per hour
+//  Setting up an array of hours of operation
+
+    StoreFront.prototype.hoursOp = ['6am', '7am', '8am','9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+//  Calculate the number of customers per hour and the number of cookies purchased per hour
 
     StoreFront.prototype.calcCustomersPerHour = function() {
-        this.customersHourly = Math.floor(Math.random * (this.customersHourlyMax - this.customersHourlyMin) + this.customersHourlyMin);
-        this.cookiesPerHour = Math.round(this.calcCustomersPerHour * this.cookiesPerHour);
 
+        return Math.floor(Math.random() * (this.customersHourlyMax - this.customersHourlyMin) + this.customersHourlyMin);
     }
+
+    StoreFront.prototype.calcCookiesPerHour = function() {
+        this.cookiesPerHour = []; 
+        this.calcCustomersPerHour();
+
+        for (var i = 0; i < this.hoursOp.length; i++) {
+        var cookies = Math.round(this.calcCustomersPerHour() * this.hourlyCookiesSold);
+            this.cookiesPerHour.push(cookies); 
+
+            console.log(cookies);
+        }
+    }
+
+
+         
+        console.log('this worked');
+    
+
+   
